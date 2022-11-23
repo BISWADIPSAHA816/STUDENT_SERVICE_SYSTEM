15e8855b14dbdf411d83d9aa0d11e602d78a7dfc
Biswadip Saha <1905871@kiit.ac.in>
2022-11-24 01:35:47 +0530

    initial commit

--- login/javascript/forget.js
+++ login/javascript/forget.js
@@ -1,9 +1,32 @@
 const emailInput = document.getElementById("emailInput");
-
+const loginredirect=document.getElementById("submit_form");
 window.addEventListener("load", afterLoadingWindow);
+window.addEventListener("click",loginredirectPage);
 
 function afterLoadingWindow() {
     if (window.localStorage.getItem("email") != null) {
         emailInput.value = window.localStorage.getItem("email");
     }
+}
+function loginredirectPage(){
+    var myHeaders = new Headers();
+myHeaders.append("verification_code", "L64d2Q");
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch("localhost:8080/auth/verify", requestOptions)
+  .then(response => response.text())
+  .then(result => {
+    alert("You have successfully Change the Password");
+    window.location.href = "./login.html";
+    
+  })
+  .catch(error =>{
+    console.log('error', error);
+    window.location.href = "./forget.html";
+  });
 }
---
+++ login/javascript/register.js
@@ -0,0 +1,25 @@
+const redirectlogin = document.getElementById("btn1");
+
+window. addEventListener("click",redirectloginPage);
+
+function redirectloginPage(){
+    var raw = "{\r\n    \"biodata\":{\r\n        \"name\":\"Test User 1\",\r\n        \"roll_no\":1905483,\r\n        \"registration_no\":1234567890,\r\n        \"email\": \"1905483@kiit.ac.in\"\r\n    },\r\n    \"credentials\":{\r\n        \"email\": \"1905483@kiit.ac.in\",\r\n        \"passphrase\":\"cleartext-password\"\r\n    }\r\n}";
+
+var requestOptions = {
+  method: 'POST',
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("localhost:8080/auth/signup", requestOptions)
+  .then(response => response.text())
+  .then(result => {
+    alert("You have Succesfully Submit");
+    window.location.href = "./login.html";
+  })
+  .catch(error => {
+    alert("Invalid Username");
+    console.log('error', error);
+    window.location.href = "./register.html";
+  });
+}
