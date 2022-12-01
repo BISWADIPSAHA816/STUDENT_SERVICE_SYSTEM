cdc16046d6caf28b26a44f241a81192a15ea0bd5
Soham Das <publicsoham.24@gmail.com>
2022-12-02 00:57:58 +0530

    response populated

--- login/javascript/overview.js
+++ login/javascript/overview.js
@@ -9,10 +9,10 @@ function closeNav() {
 
 //sidepanel functionality
 
+const issueTable = document.getElementById("issue_table");
 const overviewBtn = document.getElementById("overview_btn");
 const issuesBtn = document.getElementById("issues_btn");
 const composeBtn = document.getElementById("compose_btn");
-
 
 const overview = document.getElementById("overview");
 const issues = document.getElementById("issues");
@@ -39,6 +39,7 @@ issuesBtn.addEventListener("click", () => {
     if (issues.classList.contains("none")) {
         issues.classList.remove("none");
         issues.classList.add("flex");
+        getIssues();
     }
 
     if (overview.classList.contains("flex")) {
@@ -68,3 +69,52 @@ composeBtn.addEventListener("click", () => {
         overview.classList.add("none");
     }
 });
+
+//issues functionality
+
+const getIssues = () => {
+    let output = `
+    
+    <tr class="first_row">
+    <th>Ref No</th>
+    <th>Subject</th>
+    <th>Date Complained </th>
+    <th>Date Resolved</th>
+    <th>Status</th>
+    </tr>
+    `;
+
+    var requestOptions = {
+        method: "GET",
+        redirect: "follow",
+    };
+
+    fetch(
+            "http://ec2-65-0-120-79.ap-south-1.compute.amazonaws.com:8080/dashboard/issues?dept=Accounts",
+            requestOptions
+        )
+        .then((response) => response.json())
+        .then((result) => {
+            console.log(result);
+            result.map(item => {
+
+                output += `
+                <tr>
+                <td> ${item._id}</td>
+                <td> ${item.title}</td>
+                <td> ${getDateInYYYY_MM_DD_Format(item.created_at)}</td>
+                <td> 2022-10-25</td>
+                <td> ${item.status}</td>
+                </tr>
+                `;
+                issueTable.innerHTML = output;
+            })
+        })
+        .catch((error) => console.log("error", error));
+};
+
+
+function getDateInYYYY_MM_DD_Format(dateTime) {
+    const date = new Date(dateTime);
+    return (date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate());
+}
